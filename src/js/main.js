6f7c528f74b7f4073a7d8606918ee7ceba9dbb34
Vlad Kazakov <stalkerpelsh@gmail.com>
2018-12-13 04:26:35 +0400

    Set to default radio func

--- src/js/main.js
+++ src/js/main.js
@@ -14,3 +14,16 @@ function toggleMenu() {
         menuText[0].classList.remove("menu__text_visible");
     }
 }
+
+
+var buttons = document.querySelectorAll(".menu-comment__btn");
+
+buttons.forEach(button => {
+    button.addEventListener("click", function (event) {
+        event.preventDefault();
+        const radios = this.closest('.menu__list').querySelectorAll('.menu-item');
+        for (let i = 0; i < radios.length; i++) {
+            radios[i].querySelectorAll('.menu-item__radio')[0].checked = true;
+        }
+    })
+});
